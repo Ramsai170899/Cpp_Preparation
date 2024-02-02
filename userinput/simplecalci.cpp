8be4f3585b6204fee1356dffbfb11f2c986f1e7e
Ramsai Jagadish <ramsai.yj@gmail.com>
2024-02-02 17:23:53 +0530

    variables

---
+++ userinput/simplecalci.cpp
@@ -0,0 +1,18 @@
+// simple calculator usage of cin and cout.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "enter value of a : ";
+    cin >> a;
+    cout << "enter value of b : ";
+    cin >> b;
+
+    cout << "a+b : " << a + b << endl;
+    cout << "a-b : " << a - b << endl;
+    cout << "a*b : " << a * b << endl;
+    cout << "a/b : " << a / b << endl;
+}
---
+++ userinput/userinput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Input the number : ";
+    cin >> x;
+
+    cout << "given number is " << x << endl;
+    return 0;
+}
