c1fd2a8f3ae8aa1e82f89fcf06e8826466553631
Ramsai Jagadish <ramsai.yj@gmail.com>
2024-02-02 19:08:50 +0530

    ifelse

---
+++ ifelse/ifelse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 100;
+    if (x < 25)
+    {
+        cout << "better luck next time..... u failed";
+    }
+    else if (x > 25 & x < 60)
+    {
+        cout << "passed... well done !";
+    }
+    else
+    {
+        cout << "U rocked it....Awesome !!!";
+    }
+
+    return 0;
+}
---
+++ ifelse/shorthand.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 10;
+    (age > 18) ? cout << "U can vote" : cout << "u cannot vote";
+
+    return 0;
+}
