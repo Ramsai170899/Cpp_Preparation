8c762543d79bfdf312d1d36ef59e9ed4b391e6aa
Ramsai Jagadish <ramsai.yj@gmail.com>
2024-02-02 18:55:50 +0530

    bools

---
+++ booleans/booleans.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*int x = 30;
+    cout << (x < 100);
+
+    return 0;*/
+
+    // real life example...
+    int age = 10;
+    int votingage = 18;
+    cout << (age > votingage);
+}
