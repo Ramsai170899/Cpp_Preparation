3cf80efe1770901c30e95fcabcd76195e0bd564e
Ramsai Jagadish <ramsai.yj@gmail.com>
2024-02-02 19:26:50 +0530

    Loops

---
+++ basics/loops/dowhileloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    do
+    {
+        cout << i << " printed\n";
+        ++i;
+    } while (i <= 10);
+
+    return 0;
+}
