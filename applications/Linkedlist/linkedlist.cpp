b78f9e2ca7d48f9b179d2cc336d922f4a7db6510
Ramsai Jagadish <ramsai.yj@gmail.com>
2024-02-05 18:32:58 +0530

    ll

--- applications/Linkedlist/linkedlist.cpp
+++ applications/Linkedlist/linkedlist.cpp
@@ -1,14 +1,71 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-struct node
+class node
 {
-    int val;
+public:
+    int data;
     node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
 };
+
+void insertattail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void insertathead(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    n->data = val;
+    n->next = head;
+    head = n;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "-> ";
+        temp = temp->next;
+    }
+    cout << "null" << endl;
+}
 
 int main()
 {
-    cout << "Linked list" << endl;
+    node *head = NULL;
+    insertattail(head, 2);
+    insertattail(head, 3);
+    insertattail(head, 4);
+    insertattail(head, 5);
+    display(head);
+
+    insertathead(head, 1);
+    display(head);
+
+    return 0;
 }
