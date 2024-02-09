ce72a5847bf0feb33ca9de57abc1df01d6cacb90
Ramsai Jagadish <ramsai.yj@gmail.com>
2024-02-09 21:40:48 +0530

    cpp

--- applications/Templates/sorttemplates.cpp
+++ applications/Templates/sorttemplates.cpp
@@ -9,7 +9,7 @@ void bubbleSort(vector<T> &array)
     int n = array.size();
     for (int i = 0; i < n - 1; ++i)
     {
-        for (int j = 0; j < n - i - 1; ++j)
+        for (int j = 0; j < (n - i) - 1; ++j)
         {
             if (array[j] > array[j + 1])
             {
