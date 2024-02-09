df5acb1bcdc86015d7597feb4fb2d7478034a64e
Ramsai Jagadish <ramsai.yj@gmail.com>
2024-02-09 16:22:01 +0530

    cpp

--- applications/structuralpadding.c
+++ applications/structuralpadding.c
@@ -2,48 +2,22 @@
 
 #pragma pack(1)
 
-// char     1 byte
-//  int  2 bytes
-// int     4 bytes
-// double    8 bytes
-
-// structure A
 typedef struct structa_tag
 {
     char c;
     int s;
 } structa_t;
 
-// structure B
 typedef struct structb_tag
 {
     int s;
-    char c;
-    int i;
+    double c;
 } structb_t;
-
-// structure C
-typedef struct structc_tag
-{
-    char c;
-    double d;
-    int s;
-} structc_t;
-
-// structure D
-typedef struct structd_tag
-{
-    double d;
-    int s;
-    char c;
-} structd_t;
 
 int main()
 {
-    printf("sizeof(structa_t) = %lu\n", sizeof(structa_t));
-    printf("sizeof(structb_t) = %lu\n", sizeof(structb_t));
-    printf("sizeof(structc_t) = %lu\n", sizeof(structc_t));
-    printf("sizeof(structd_t) = %lu\n", sizeof(structd_t));
+    printf("sizeof(structa_t) = %lu \n", sizeof(structa_t));
+    printf("sizeof(structb_t) = %lu \n", sizeof(structb_t));
 
     return 0;
 }
